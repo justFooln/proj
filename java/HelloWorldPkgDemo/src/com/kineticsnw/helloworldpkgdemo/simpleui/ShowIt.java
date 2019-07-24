44e6bd2d9b1007e28b7f4ec37e099269d0e01dee
JohnHake <john.hake@jetbrains.com>
2019-07-24 15:03:44 -0700

    capture misc

--- java/HelloWorldPkgDemo/src/com/kineticsnw/helloworldpkgdemo/simpleui/ShowIt.java
+++ java/HelloWorldPkgDemo/src/com/kineticsnw/helloworldpkgdemo/simpleui/ShowIt.java
@@ -1,15 +1,30 @@
 package com.kineticsnw.helloworldpkgdemo.simpleui;
 
 
+import java.util.Date;
 public class ShowIt {
 
   public ShowIt() {
 
   }
 
+  String str1 = "I am a string";
+  String str2 = "String number two";
+  Date date1 = new Date();
+  Date date2 = new Date();
+
   // Now display the text
   public void showNow() {
-    System.out.println( "Printing from ShowIt.showNow" );
+
+    if (str1==str2) {
+
+
+      if ( date1 != date2 ) {
+        boolean foo = true;
+        String name = foo ? "JOHN" : "john";
+        System.out.println( "Printing from ShowIt.showNow" ) ;
+      }
+    }
   }
 
 
