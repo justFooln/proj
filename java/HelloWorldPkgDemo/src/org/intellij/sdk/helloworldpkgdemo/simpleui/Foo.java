3ea2e36ba65fe769dd2d78fe395dc6502272303d
JohnHake <john.hake@jetbrains.com>
2019-07-27 11:58:56 -0700

    Use logical pos

--- java/HelloWorldPkgDemo/src/org/intellij/sdk/helloworldpkgdemo/simpleui/Foo.java
+++ java/HelloWorldPkgDemo/src/org/intellij/sdk/helloworldpkgdemo/simpleui/Foo.java
@@ -1,7 +1,7 @@
-package org.intellij.sdk.helloworldpkgdemo.simpleui;  // Editor Line 0
-// Editor Line 1
-// Editor Line 2
-// Editor Line 3 - This is a long line  This is a long line  This is a long line  This is a long line  This is a long line  This is a long line.
-// Editor Line 4
-public class Foo {     // Editor Line 5
+// Logical Line 0
+// Logical Line 1
+// Logical Line 2
+// Logical Line 3 - This is a long line  This is a long line  This is a long line  This is a long line  This is a long line  This is a long line.                                                    // E3 NEW V4
+package org.intellij.sdk.helloworldpkgdemo.simpleui;                                    // Logical Line 4
+public class Foo {                                                                      // Logical Line 5
 }
