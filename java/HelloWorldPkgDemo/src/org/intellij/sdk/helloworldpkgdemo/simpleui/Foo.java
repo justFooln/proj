a18af57efd7d7bbb55fd170d8f2704ad9c008b78
JohnHake <john.hake@jetbrains.com>
2019-09-05 17:46:05 -0700

    Cleaner

--- java/HelloWorldPkgDemo/src/org/intellij/sdk/helloworldpkgdemo/simpleui/Foo.java
+++
@@ -1,7 +0,0 @@
-// Logical Line 0
-// Logical Line 1
-// Logical Line 2
-// Logical Line 3 - This is a long line  This is a long line  This is a long line  This is a long line  This is a long line  This is a long line.                                                    // E3 NEW V4
-package org.intellij.sdk.helloworldpkgdemo.simpleui;                                    // Logical Line 4
-public class Foo {                                                                      // Logical Line 5
-}
